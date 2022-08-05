d34ce599bdc7ee559e50fa5b20a45d14a74f826d
ssxy <ay18736219230@163.com>
2022-08-05 09:43:20 +0800

    master

---
+++ myVscode/javaScript/11、第十一天/js/city.js
@@ -0,0 +1,64 @@
+var citys = {
+		"districts": [{	
+				"adcode": "100000",
+				"name": "中华人民共和国",
+				"districts": [{
+						"adcode": "410000",
+						"name": "河南省",
+						"districts": [{
+								"citycode": "0379",
+								"adcode": "410300",
+								"name": "洛阳市",
+								"center": "112.434468,34.663041",
+								"level": "city",
+								"districts": [{
+									"citycode": "0379",
+									"adcode": "410323",
+									"name": "新安县",
+									"center": "112.141403,34.728679",
+									"level": "district",
+									"districts": []
+								}]
+							},
+							{
+								"citycode": "0398",
+								"adcode": "411200",
+								"name": "三门峡市",
+								"center": "111.194099,34.777338",
+								"level": "city",
+								"districts": [{
+									"citycode": "0398",
+									"adcode": "411221",
+									"name": "渑池县",
+									"center": "111.762992,34.763487",
+									"level": "district",
+									"districts": []
+								}]
+							}
+						]
+					},
+					{
+						"citycode": [],
+						"adcode": "440000",
+						"name": "广东省",
+						"center": "113.280637,23.125178",
+						"level": "province",
+						"districts": [{
+							"citycode": "0754",
+							"adcode": "440500",
+							"name": "汕头市",
+							"center": "116.708463,23.37102",
+							"level": "city",
+							"districts": [{
+								"citycode": "0754",
+								"adcode": "440513",
+								"name": "潮阳区",
+								"center": "116.602602,23.262336",
+								"level": "district",
+								"districts": []
+							}]
+						}]
+					}]
+
+		    }]			
+		}
---
+++ myVscode/javaScript/12、第十二天/js/json.js
@@ -0,0 +1,102 @@
+
+var obj1 = {
+				"imgComments": {
+					"imgList": [{
+							"imageId": 698391890,
+							"content": "操作简单方便，使用舒适感强，声音小?",
+							"creationTime": "2018-11-01 23:51:40",
+							"nickname": "j***9",
+							"productColor": "5050CC",
+							"userLevelName": "银牌会员",
+							"userClientShow": "来自京东Android客户端",
+						},
+						{
+							"imageId": 698391889,
+							"content": "还不错，老婆很喜欢",
+							"creationTime": "2018-10-05 23:51:40",
+							"nickname": "h***2",
+							"productColor": "C550C0",
+							"userLevelName": "钻石会员",
+							"userClientShow": "来自京东iPhone客户端",
+						}
+					]
+				}
+			}
+var obj2={
+	"男明星": [
+			{ "name":"梁朝伟","age":"40"},
+			{ "name":"刘德华","age":"42"},
+			{ "name":"彭于晏","age":"29"}
+		],
+	"女明星": [
+			{ "name":"刘嘉玲","age":"40"},
+			{ "name":"张敏","age":"41"},
+			{ "name":"张柏芝","age":"30"},
+			{ "name":"朱茵","age":"38"}
+		]
+}
+
+
+var user ={
+		"employees":[  
+                    {
+			            "name":"shenmiweiyi",
+			            "QQ":306451129,
+			            "email":"shenmiweiyi@163.com",
+			            "address":
+                                [
+			                  		{"City":"ZhengZhou","ZipCode":"450000"},
+			                  		{"City":"BeiJing","ZipCode":"100000"}
+			                	]
+			         },
+			         {
+			            "name":"kehao",
+			            "QQ":254892313,
+			            "email":"kehao@163.com",
+			            "address":
+			            [
+			                  {"City":"ShangHai","ZipCode":"200000"},
+			                  {"City":"GuangZhou","ZipCode":"510000"}
+			            ]
+			       }
+			    ]
+        }
+		
+		
+var scores = [{
+					"id": 1,
+					name: "张三",
+					score: 55,
+					subject: "语文"
+				},
+				{
+					"id": 2,
+					name: "李四",
+					score: 76,
+					subject: "语文"
+				},
+				{
+					"id": 3,
+					name: "王五",
+					score: 67,
+					subject: "语文"
+				},
+				{
+					"id": 4,
+					name: "张三",
+					score: 23,
+					subject: "数学"
+				},
+				{
+					"id": 5,
+					name: "李四",
+					score: 81,
+					subject: "数学"
+				},
+				{
+					"id": 6,
+					name: "王五",
+					score: 64,
+					subject: "数学"
+				},
+			]
